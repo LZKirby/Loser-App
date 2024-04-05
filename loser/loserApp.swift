bbccd7cbfcaad28b226f3d95d2c4bbe32dd2c726
LZKirby <148351938+LZKirby@users.noreply.github.com>
2024-04-05 12:28:03 -0400

    Initial Commit

---
+++ loser/loserApp.swift
@@ -0,0 +1,32 @@
+//
+//  loserApp.swift
+//  loser
+//
+//  Created by leiland kirby on 4/4/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct loserApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
